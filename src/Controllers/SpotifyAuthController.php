8dfbb2d7fd2aaf8487d086701fea0c795cc66bd3
emmpaul <95937505+emmpaul@users.noreply.github.com>
2026-04-08 22:40:41 +0000

    Fix styling

--- src/Controllers/SpotifyAuthController.php
+++ src/Controllers/SpotifyAuthController.php
@@ -9,12 +9,13 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Log;
 use Laravel\Socialite\Facades\Socialite;
+use Laravel\Socialite\Two\SpotifyProvider;
 
 class SpotifyAuthController extends Controller
 {
     public function redirect(): RedirectResponse
     {
-        /** @var \Laravel\Socialite\Two\SpotifyProvider $driver */
+        /** @var SpotifyProvider $driver */
         $driver = Socialite::driver('spotify');
 
         return $driver
